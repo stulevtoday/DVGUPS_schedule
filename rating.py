00151f98a9ea53b63895204c26c72aea7a497780
stulevtoday <danilstulev@gmail.com>
2022-10-04 16:34:51 +1000

    selenium parse

--- rating.py
+++ rating.py
@@ -15,11 +15,35 @@ driver = webdriver.Chrome(
 driver.get('https://www.dvgups.ru/studtopmenu/student-rating')
 sleep(2)
 
-try:
-    iframe = driver.find_element(By.ID, 'blockrandom')
+def click_iframe():
+    iframe = driver.find_element(By.NAME, 'iframe')
     driver.switch_to.frame(iframe)
+
+try:
+    # todo: auth
+    click_iframe()
     driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_LinkButtonFind').click()
+
     sleep(2)
+
+    # need group name
+    driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_TextBoxGroup').send_keys('БО211ПИН')
+    driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_Button1').click()
+
+    sleep(3)
+
+    driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_GridView1_ctl02_LinkButton1').click()
+
+    sleep(3)
+
+    # need fullname
+    driver.find_element(By.LINK_TEXT, 'Стулёв Данил Евгеньевич').click()
+
+    sleep(5)
+
+    driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_ASPxGridView1_DXMainTable').screenshot('person_rating.png')
+
+    sleep(3)
 
 except Exception as ex:
     print(ex)
