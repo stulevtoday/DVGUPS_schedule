6fbcbc26a13a1acb5cc45f932233339401abe076
firug <firuz2004001@mail.ru>
2022-10-04 15:05:31 +1000

    create bot_main

---
+++ bot_main.py
@@ -0,0 +1,28 @@
+# library for logging activities
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+from settings import API_TOKEN
+
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(message: types.Message):
+	"""
+	This handler will be called when user sends 
+	"/start" and "/help"
+	"""
+	await message.reply("Hi!\nI am FESTU shedule bot")
+
+@dp.message_handler()
+async def echo(message:types.Message):
+	await message.answer(message.text)
+
+
+if __name__ == "__main__":
+	executor.start_polling(dp, skip_updates=True)
