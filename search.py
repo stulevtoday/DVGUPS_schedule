7069bb5a65f539c1373f4ce36f581d528b7e8cca
SlavicSquat <developer@yandex.ru>
2022-10-07 17:53:46 +1000

    Search as function

--- search.py
+++ search.py
@@ -1,13 +1,9 @@
-import sqlite3
-
-db = sqlite3.connect('main_DB')
-cursor = db.cursor()
-
-print('Введите сокращённое название своей группы(пример - БО211ПИН)')
-user_group = input()
-
-id_group, id_fac = cursor.execute("""select id, id_fac from group_to_facs where name = ?""", (user_group, )).fetchall()[0]
-print('Номер группы ->', id_group)
-print('Номер института ->', id_fac)
-db.commit()
-db.close()
+def group_parse(user_group):
+    import sqlite3
+    db = sqlite3.connect('main_DB')
+    cursor = db.cursor()
+    id_group, id_fac = \
+    cursor.execute("""select id, id_fac from group_to_facs where name = ?""", (user_group,)).fetchall()[0]
+    return id_group, id_fac
+    db.commit()
+    db.close()
